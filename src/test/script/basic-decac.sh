ec045eb0bef5334a2b32c5258b0e4fa2f1620024
Loan Gatimel <loan.gatimel@grenoble-inp.org>
2024-01-22 18:05:10 +0100

    jacoco

--- src/test/script/basic-decac.sh
+++ src/test/script/basic-decac.sh
@@ -3,6 +3,10 @@
 # Test de l'interface en ligne de commande de decac.
 # On ne met ici qu'un test trivial, a vous d'en ecrire de meilleurs.
 
+
+# Se déplacer dans le répertoire du script
+cd "$(dirname "$0")"/../../.. || exit 1
+
 PATH=./src/main/bin:"$PATH"
 
 decac_moins_b=$(decac -b)
@@ -25,7 +29,7 @@ fi
 echo "Pas de probleme detecte avec decac -b."
 
 
-decac_moins_p=$(decac -p ../deca/context/valid/provided/hello-world.deca )
+decac_moins_p=$(decac -p src/test/deca/context/valid/provided/hello-world.deca )
 
 if [ "$?" -ne 0 ]; then
     echo "ERREUR: decac -p a termine avec un status different de zero."
@@ -44,7 +48,7 @@ fi
 
 echo "Pas de probleme detecte avec decac -p."
 
-decac_moins_v=$(decac -v ../deca/context/valid/provided/hello-world.deca )
+decac_moins_v=$(decac -v src/test/deca/context/valid/provided/hello-world.deca )
 
 if [ "$?" -ne 0 ]; then
     echo "ERREUR: decac -v a termine avec un status different de zero."
@@ -63,7 +67,7 @@ fi
 
 echo "Pas de probleme detecte avec decac -v."
 
-decac_moins_n=$(decac -n ../deca/context/valid/provided/hello-world.deca )
+decac_moins_n=$(decac -n src/test/deca/context/valid/provided/hello-world.deca )
 
 if [ "$?" -ne 0 ]; then
     echo "ERREUR: decac -n a termine avec un status different de zero."
@@ -77,7 +81,7 @@ fi
 
 echo "Pas de probleme detecte avec decac -n."
 
-decac_moins_r=$(decac -r 5 ../deca/context/valid/provided/hello-world.deca )
+decac_moins_r=$(decac -r 5 src/test/deca/context/valid/provided/hello-world.deca )
 
 if [ "$?" -ne 0 ]; then
     echo "ERREUR: decac -r a termine avec un status different de zero."
@@ -92,7 +96,7 @@ fi
 
 echo "Pas de probleme detecte avec decac -r."
 
-decac_moins_d=$(decac -d ../deca/context/valid/provided/hello-world.deca )
+decac_moins_d=$(decac -d src/test/deca/context/valid/provided/hello-world.deca )
 
 if [ "$?" -ne 0 ]; then
     echo "ERREUR: decac -d a termine avec un status different de zero."
@@ -111,7 +115,7 @@ fi
 
 echo "Pas de probleme detecte avec decac -d."
 
-decac_moins_P=$(decac -P ../deca/context/valid/provided/hello-world.deca )
+decac_moins_P=$(decac -P src/test/deca/context/valid/provided/hello-world.deca )
 
 if [ "$?" -ne 0 ]; then
     echo "ERREUR: decac -P a termine avec un status different de zero."
