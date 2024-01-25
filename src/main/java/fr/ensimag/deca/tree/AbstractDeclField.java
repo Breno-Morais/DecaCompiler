f3502689b6b5fad7a903c0c26f879a7f3e5a8817
Breno Morais <brneo.da-silva-morais@grenoble-inp.org>
2024-01-25 17:12:08 +0100

    Asin and Atan error messages

--- src/main/java/fr/ensimag/deca/tree/AbstractDeclField.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractDeclField.java
@@ -10,14 +10,15 @@ public abstract class AbstractDeclField extends Tree {
      * Pass 2 of [SyntaxeContextuelle]. Verify that the class members (fields and
      * methods) are OK, without looking at method body and field initialization.
      */
-    protected abstract void verifyClassMembers(DecacCompiler compiler, ClassDefinition superClass, ClassDefinition classe, int index)      //TODO pas sur des paramètres dedans
+    protected abstract void verifyClassMembers(DecacCompiler compiler, ClassDefinition superClass,
+                                               ClassDefinition classe, int index)                                                            //TODO pas sur des paramètres dedans
             throws ContextualError;
 
     /**
      * Pass 3 of [SyntaxeContextuelle]. Verify that instructions and expressions
      * contained in the class are OK.
      */
-    protected abstract void verifyClassBody(DecacCompiler compiler, EnvironmentExp env_exp, ClassDefinition classe)         //TODO pas sur des paramètres dedans
+    protected abstract void verifyClassBody(DecacCompiler compiler, EnvironmentExp env_exp, ClassDefinition classe)                        //TODO pas sur des paramètres dedans
             throws ContextualError;
 
     public abstract AbstractVisibility getVisibility();
